649c9083d7099f01bdc45e07794f30d36a6068a1
rfsyhb <rafisyihab3@gmail.com>
2024-03-25 12:31:39 +0700

    PropTypes class

--- src/concepts/PropTypesApp.jsx
+++ src/concepts/PropTypesApp.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from 'prop-types';
 
+// functional component
 function SayHello({ name }) {
   return <p>Hello, {name}</p>
 }
@@ -9,8 +10,24 @@ SayHello.propTypes = {
   name: PropTypes.string.isRequired
 }
 
+// class component
+class SayWorld extends React.Component {
+  render () {
+    const { word } = this.props;
+
+    return <p>Hello, {word}</p>
+  }
+}
+
+SayWorld.propTypes = {
+  word: PropTypes.string.isRequired
+}
+
 export default function PropTypesApp() {
   return (
-    <SayHello name={["asep", "gaming"]} />
+    <>
+      {/* <SayHello name={["asep", "gaming"]} /> */}
+      <SayWorld word={["asep", "gaming"]} />
+    </>
   )
 }
