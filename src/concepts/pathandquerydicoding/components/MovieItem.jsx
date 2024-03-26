3bc3064bc8d5dc389f2d5c4cc7e128b148d046d9
rfsyhb <rafisyihab3@gmail.com>
2024-03-26 14:37:39 +0700

    Meambah Link pada MovieItem
    Link digunakan agar judul dapat ditekan untuk membuka detail movie tersebut

--- src/concepts/pathandquerydicoding/components/MovieItem.jsx
+++ src/concepts/pathandquerydicoding/components/MovieItem.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom';
+
+// Tugas 1: Menambah Link agar Judul bisa membuka detail Movie sesuai id
 
 function MovieItem({ id, title, backdropPath, overview }) {
   return (
     <article>
       <img src={backdropPath} alt={title} />
-      <h3>{title}</h3>
+      {/* <h3>{title}</h3> */}
+      <h3>
+        <Link to={`/movies/${id}`}>{title}</Link>
+      </h3>
       <p>{overview}</p>
     </article>
   )
